64969aa6f3e581f143b4e2cedb84ab306ea16044
olielbertochi07 <62772807+olielbertochi07@users.noreply.github.com>
2023-11-19 20:20:12 -0300

    Add files via upload

--- app_lista_presupuestos.py
+++ app_lista_presupuestos.py
@@ -39,11 +39,16 @@ def Filtrar_Presupuesto():
             else:
                 e = "0" 
             temp_color = "green" if row[6] == {'0'} else "none"
-            st.markdown(f'<div style="color:{temp_color};"> FECHA: {row[4]} </div>', unsafe_allow_html=True)
-            st.markdown(f'<div style="color:{temp_color}; margin-top: 5px;"> CLIENTE: {row[2]} </div>', unsafe_allow_html=True)
-            st.markdown(f'<div style="color:{temp_color}; margin-top: 5px;"> VENDEDOR: {row[3]} </div>', unsafe_allow_html=True)
-            valor_f ="TOTAL PRES.: " + "{:,} Gs".format(int(row[5])).replace(",", ".")     
-            st.markdown(f'<div style="float: right;color:{temp_color}; margin-top: 20px;margin-bottom: -20px;margin-right: 10px;">{valor_f}</div>', unsafe_allow_html=True)
+
+            st.markdown(f'<div style="float: left; margin-top: 10px; color:{temp_color};"> Fecha: {row[4]} </div> <div style="float: right; margin-top: 10px;color:{temp_color};">Hora: {row[8]}</div>', unsafe_allow_html=True)
+            st.markdown(f'<div style="float: left; color:{temp_color}; margin-top: 0px;"> Cliente: {row[2]} </div>', unsafe_allow_html=True)
+            st.markdown(f'<div style="float: left; color:{temp_color}; margin-top: 0px;"> Vendedor: {row[3]} </div>', unsafe_allow_html=True)
+            if row[7] !='':
+                st.markdown(f'<div style="float: left; color:{temp_color}; margin-top: 0px;"> Observación: {row[7]} </div>', unsafe_allow_html=True)
+            
+            valor_f ="Total Pres.: " + "{:,} Gs".format(int(row[5])).replace(",", ".")     
+            st.markdown(f'<div style="float: right;color:{temp_color}; margin-top: 0px;margin-bottom: -10px;margin-right: 10px;">{valor_f}</div>', unsafe_allow_html=True)
+           
             if e != "0":
                  with st.form(key='my_form'):
                     st.markdown(f'<div style="float: center;color:{temp_color}; margin-top: 5px;margin-bottom: -30px;">Items Presupuesto Nº: {row[0]}</div>', unsafe_allow_html=True)
@@ -52,21 +57,23 @@ def Filtrar_Presupuesto():
                     total = 0
                     for row in rst:                
                         id_format = (f'ID: {row[0]}')
-                        codigo_format = (f'{row[1]}')
-                        st.markdown(f'<div style="float: left;color:{temp_color};">{id_format}</div>', unsafe_allow_html=True)
-                        st.markdown(f'<div style="float: left;color:{temp_color};">{codigo_format}</div>', unsafe_allow_html=True)     
+                        codigo_format = (f'CODIGO: {row[1]}')
+
+                        st.markdown(f'<div style="float: left;color:{temp_color};">{id_format}</div> <div style="float: right;color:{temp_color};">{codigo_format}</div>', unsafe_allow_html=True)
+
                         descripcion_format = (f'{row[2]}')
                         st.markdown(f'<div style="float: left;color:{temp_color};">{descripcion_format}</div>', unsafe_allow_html=True)
                         ctd_formatted = "<span>Cantidad: {:,} Und</span>".format(int(row[3])).replace(",", ".")
                         pc_formatted = "Precio: " + "{:,} Gs".format(int(row[4])).replace(",", ".")
                         tl_formatted ="Total: " + "{:,} Gs".format(int(row[5])).replace(",", ".")  
-                        st.markdown(f'<div style="display: inline-block; float: left; margin-right: 40px; color:{temp_color};">{ctd_formatted}</div>', unsafe_allow_html=True)
+                        st.markdown(f'<div style="display: inline-block; float: left; margin: 0 auto; color:{temp_color};">{ctd_formatted}</div>', unsafe_allow_html=True)
                         st.markdown(f'<div style="display: inline-block; float: left; margin-right: 40px; color:{temp_color};">{pc_formatted}</div> <div style="display: inline-block; float: right; color:{temp_color};">{tl_formatted}</div>', unsafe_allow_html=True)
                         total +=  row[5]    
-                        st.markdown('<hr class="linha-cinza";color:{temp_color}; margin-top: -100 px;margin-bottom: -30px;>', unsafe_allow_html=True) 
+                        st.markdown('---')
+
                     total_formatted = "Total: " + "{:,} Gs".format(int(total)).replace(",", ".")
                     st.markdown(f'<div style="float: right;color:{temp_color};">{total_formatted}</div>', unsafe_allow_html=True)
-                    if st.form_submit_button('Volver ↑'):
+                    if st.form_submit_button('Volver ↑','Click Para Ocultar Detalles'):
                         d = "0"
                     
             st.markdown("<style> .linha-presupuesto {border: 0.1px solid burlywood;margin: 20px 0;}</style>",unsafe_allow_html=True)
